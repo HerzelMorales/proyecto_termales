79dc3bcf9f674b495328e3fef4ad6b88c6baea22
Herzel Morales <moralesjoel227@gmail.com>
2021-05-06 11:30:08 -0400

    Initialization for thermal proyect repository.

---
+++ helpers/footer.php
@@ -0,0 +1,19 @@
+ <footer id="footerthang" role="contentinfo">
+      <section class="container-fluid text-center">
+        <p>
+          <small style="display: block;">
+            "© 20<?php echo date('y'); ?>" <a href>Genesis Security Services, Inc.</a>
+          </small>
+          <small style="display: block;"></small>
+        </p>
+      </section>
+    </footer>
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+    <script src="./js/customscripts.js"></script>
+
+  </body>
+</html>
